574f194f5e876c3e5a82ee9c8fe30e117ea8d385
MohamedOkasha10 <166031895+MohamedOkasha10@users.noreply.github.com>
2024-07-13 03:29:42 +0300

    Add files via upload

---
+++ pattern_2.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int m;
+int k;
+
+int main()
+{
+     for(m=1 ; m<=5 ; m++)
+    {
+        for(k=1 ; k<=5 ; k++)
+        {
+            printf("%i", m);
+        }
+        printf("\n");
+    }
+
+
+    return 0;
+}
