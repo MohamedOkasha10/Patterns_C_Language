844def5324ed4eb149f6a17838389ed464075eee
MohamedOkasha10 <166031895+MohamedOkasha10@users.noreply.github.com>
2024-07-13 03:27:02 +0300

    Add files via upload

---
+++ Solid_Rectangular_Star.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int k; // k => Rows
+int m; // m => Columns
+
+
+int main()
+{
+    for(k=1 ; k<=3 ; k++)
+    {
+        for(m=1 ; m<=5 ; m++)
+        {
+            printf("*");
+        }
+        printf("\n");
+    }
+
+
+
+
+    return 0;
+}
